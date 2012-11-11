13d27faece9216d14db348a208885252c9384f36
me <me@me.(none)>
2012-11-11 10:31:43 -0800

    update scaffold for users and posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120805222457) do
+ActiveRecord::Schema.define(:version => 20121111182057) do
+
+  create_table "posts", :force => true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
